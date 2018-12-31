68d528d590c109674c0b1e1f2c4131c0073e78d5
dom huh <40368238+domhuh@users.noreply.github.com>
2018-12-31 17:46:03 -0500

    Changed js

--- js/main.js
+++ js/main.js
@@ -88,11 +88,9 @@ var TextScramble = function () {
 // ——————————————————————————————————————————————————
 
 var phrases = [
-  'Happy Holidays!!!',
   'Welcome to GMU IEEE',
   'We focus on professional development',
-  'We pursue research in technology',
-  'Happy Holidays!!!'
+  'We pursue research in technology'
 ];
 
 var el = document.querySelector('.text');
